0678653b1d1cd7f59a9d9dfcadc93b4b47f47b1b
Mills McIlroy <37815163+millsmcilroy@users.noreply.github.com>
2023-02-06 17:17:19 -0700

    Refactor directory structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PricesController } from './prices.controller';
-import { PricesService } from './prices.service';
+import { PricesController } from './prices/prices.controller';
+import { PricesService } from './prices/prices.service';
 
 @Module({
   imports: [],

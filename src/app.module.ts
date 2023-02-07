6fa7849f3660e9647a3078a0fa5152206548b15b
Mills McIlroy <37815163+millsmcilroy@users.noreply.github.com>
2023-02-06 20:51:57 -0700

    Add sequelize for postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PricesController } from './prices/prices.controller';
 import { PricesService } from './prices/prices.service';
+import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
-  imports: [],
+  imports: [SequelizeModule.forRoot({
+    dialect: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'user',
+    password: 'password',
+    database: 'cryptomarketcap',
+    models: [],
+  }),],
   controllers: [AppController, PricesController],
   providers: [AppService, PricesService],
 })

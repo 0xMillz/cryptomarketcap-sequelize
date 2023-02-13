ddd9c00290b3007626b68e7bd766d75e82f4899b
Mills McIlroy <37815163+millsmcilroy@users.noreply.github.com>
2023-02-12 17:00:44 -0700

    Add db connection log to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,29 +1,40 @@
+import 'dotenv/config';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AssetsModule } from './modules/assets/assets.module';
-import { SequelizeModule } from "@nestjs/sequelize";
+import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: `.env.${process.env.NODE_ENV}`
+      envFilePath: `.env.${process.env.NODE_ENV}`,
     }),
     SequelizeModule.forRootAsync({
-      imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: (config: ConfigService) => ({
-          username: config.get('POSTGRES_USER'),
-          password: config.get('POSTGRES_PASSWORD'),
-          database: config.get('POSTGRES_NAME'),
-          host: config.get('POSTGRES_HOST'),
-          port: +config.get('POSTGRES_PORT'),
-          dialect: config.get('POSTGRES_DIALECT')
-        })
+      useFactory: (config: ConfigService) => {
+        const username = config.get('POSTGRES_USER') ?? 'postgres';
+        const password = config.get('POSTGRES_PASSWORD') ?? 'password';
+        const host = config.get('POSTGRES_HOST') ?? 'localhost';
+        const port = config.get('POSTGRES_PORT') ?? 5432;
+        const database = config.get('POSTGRES_NAME') ?? 'cryptomarketcap';
+        const dialect = config.get('POSTGRES_DIALECT') ?? 'postgres';
+
+        console.info(`Attempting connection to ${database} db on port ${port}`);
+
+        return {
+          username,
+          password,
+          database,
+          host,
+          port: +port,
+          dialect,
+        };
+      },
     }),
-    AssetsModule
+    AssetsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/modules/assets/assets.module.ts
+++ src/modules/assets/assets.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AssetsService } from './assets.service';
-import { SequelizeModule } from "@nestjs/sequelize";
-import { Asset } from "./asset.entity";
-import { AssetsController } from "./assets.controller";
+import { SequelizeModule } from '@nestjs/sequelize';
+import { Asset } from './asset.entity';
+import { AssetsController } from './assets.controller';
 
 @Module({
   imports: [SequelizeModule.forFeature([Asset])],
